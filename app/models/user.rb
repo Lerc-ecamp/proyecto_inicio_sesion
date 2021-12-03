f9a30c4e293c4d969bd6824f79f69283f6ee8ded
Lerc-ecamp <lilian.rozas@gmail.com>
2021-12-02 23:28:29 -0300

    User (core)
    Core con pruebas terminadas

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+    validates :first_name,:last_name, presence: true, length: { in: 2..20}
+    validates :age, presence: true, numericality: { only_integer: true, in: 10..150 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+end
